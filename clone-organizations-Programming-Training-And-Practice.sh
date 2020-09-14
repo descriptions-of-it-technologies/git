c73a778ad1204d85fc51fa32f99d7f65ebd9fb87
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-09-14 14:57:33 +0200

    Adding new information.

--- clone-organizations-Programming-Training-And-Practice.sh
+++ clone-organizations-Programming-Training-And-Practice.sh
@@ -3,4 +3,8 @@
 ORGANIZATION_NAME={Programming-Training-And-Practice}
 
 # Must be a pre-installed package jb.
-curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?per_page\=200 | jq -r '.[].clone_url' | xargs -n 1 git clone
+# Download repositories via https.
+#curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?per_page\=200 | jq -r '.[].clone_url' | xargs -n 1 git clone
+
+# Download repositories via ssh.
+curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?per_page\=200 | jq -r '.[].ssh_url' | xargs -n 1 git clone
