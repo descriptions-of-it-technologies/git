3c3e58facd5c24d6ca400d13e05d62a7fa5be197
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-10-05 22:00:17 +0200

    Adding new Information.

--- clone-organizations-descriptions-of-it-technologies.sh
+++ clone-organizations-descriptions-of-it-technologies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ORGANIZATION_NAME={Programming-Training-And-Practice}
+ORGANIZATION_NAME={descriptions-of-it-technologies}
 
 # Must be a pre-installed package jb.
 # Download repositories via https.
 #curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?per_page\=200 | jq -r '.[].clone_url' | xargs -n 1 git clone
 
+# Must be a pre-installed package jb.
 # Download repositories via ssh.
 curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?per_page\=200 | jq -r '.[].ssh_url' | xargs -n 1 git clone
