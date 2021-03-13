8992d5d675a05bb31655fc12d3ceb3669ad6ca71
Roman Tsyupryk <roman.tsyupryk@asesormasmovil.es>
2021-03-13 09:40:10 +0100

    Adding new information.

--- clone-organization-descriptions-of-it-technologies.sh
+++ clone-organization-descriptions-of-it-technologies.sh
@@ -6,11 +6,11 @@ mkdir -p $ORGANIZATION_NAME
 
 cd $ORGANIZATION_NAME
 
-# Must be a pre-installed package jb.
+# Must be a pre-installed package jq.
 # Download repositories via https.
 #curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?per_page\=200 | jq -r '.[].clone_url' | xargs -n 1 git clone
 
-# Must be a pre-installed package jb.
+# Must be a pre-installed package jq.
 # Download repositories via ssh.
 # Not download private repositories.
 curl -s https://api.github.com/orgs/$ORGANIZATION_NAME/repos\?page\=1\&per_page\=100 | jq -r '.[].ssh_url' | xargs -n 1 git clone
